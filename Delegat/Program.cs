4871f4e42b6a3c825452e075ff7ad4004fafe0b9
OvchinnikovStepan <124854061+OvchinnikovStepan@users.noreply.github.com>
2023-05-14 00:36:18 +0600

    Update Program.cs

--- Delegat/Program.cs
+++ Delegat/Program.cs
@@ -1,6 +1,7 @@
-﻿namespace Delegat
+namespace Delegat
 {
-   
+
+    public delegate void Bob(int r);
         public class Car
         {
             public string numer { get; set; }
@@ -25,14 +26,14 @@
                 Console.WriteLine("Помыли машину {0} {1}", garage.cars[num].mark, garage.cars[num].numer);
             }
 
-             public delegate void Bob(int r);
+             
 
 
             public void WashAll(Bob bob)
             {
                for (int i = 0;i< garage.cars.Count;i++)
                 {
-                    Wash(i);
+                    bob(i);
                 }
             }
         }
@@ -40,7 +41,21 @@
     {
         static void Main(string[] args)
         {
-           
+            Car car1 = new Car { mark = "Lada", numer = "55551" };
+            Car car2 = new Car { mark = "Honda", numer = "12345" };
+            Car car3 = new Car { mark = "Lamba", numer = "41242" };
+            Garage garage = new Garage();
+            garage.Add(car1);
+            garage.Add(car2);
+            garage.Add(car3);
+            CarWash carWash = new CarWash(garage);
+            carWash.Wash(1);
+            Console.WriteLine();
+            Bob bob=carWash.Wash;
+            carWash.WashAll(bob);
+            garage.Remove(car3);
+            Console.WriteLine();
+            carWash.WashAll(bob);
         }
     }
 }
